ee915a5d69d294f7dd6f1ba020445cf164109b88
Steven Boulom <sboulom@gmail.com>
2020-09-11 22:07:03 -0700

    milk function

--- scripts.js
+++ scripts.js
@@ -11,3 +11,34 @@ function lifeInWeeks(age){
 }
 
 lifeInWeeks(12);
+
+function getMilk (money) {
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+
+    var numberOfBottles = Math.floor(money /1.5);
+
+    console.log("buy " + numberOfBottles + " bottles of milk! ")
+
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+
+    return money % 1.5;
+}
+
+var change = getMilk(4);
+console.log(change);
