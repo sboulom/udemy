582bfcfa9df68af30e3443e28fe1c340bbd0bf5e
Steven Boulom <sboulom@gmail.com>
2020-09-22 12:09:03 -0700

    fixed fib gen

--- scripts.js
+++ scripts.js
@@ -206,24 +206,24 @@
 //  }
 //  fizzBuzz();
 
-// fibonacciGenerator
+fibonacciGenerator
+ 
+function fibonacciGenerator(n){
 
-// function fibonacciGenerator(n){
+    var output = [];
+    if (n === 1){
+        output = [0];
+    } else if (n === 2){
+        output = [0,1]
+    } else {
+        output = [0,1];
 
-//     var output = [];
-//     if (n === 1){
-//         output = [0];
-//     } else if (n === 2){
-//         output = [0,1]
-//     } else {
-//         output = [0,1];
+        for (var i = 2; i < n; i++){
+        output.push(output[output.length - 2] + output[output.length - 1]);
+        }
+    }
+    return output;
+}
 
-//         for (var i = 2; i < n; i++){
-//         output.push(output[output.length - 2] + output[output.length - 1]);
-//         }
-//     }
-//     return output;
-// }
-
-// output = fibonacciGenerator();
-// console.log(output);
+output = fibonacciGenerator();
+console.log(output);
