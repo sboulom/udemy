539154d0853a49590e11e958b1cc546e319baefa
Steven Boulom <sboulom@gmail.com>
2020-09-26 22:12:45 -0700

    JS practice

--- DOM/index.js
+++ DOM/index.js
@@ -1,1 +1,13 @@
 document.querySelector("button").style.backgroundColor = "red";
+
+document.querySelector("h1").classList.add("huge");
+document.querySelector("h1").classList.add("huge");
+document.querySelector("h1").classList.add("huge");
+document.querySelector("h1").classList.add("huge");
+document.querySelector("h1").classList.add("huge");
+document.querySelector("h1").classList.add("huge");
+
+document.querySelector("a").getAttribute("href");
+document.querySelector("href").setAttribute("href", "www.yahoo.com");
+
+
