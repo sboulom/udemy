d89c4698c20e1179703117c8ac0a4eaee4677ed5
Steven Boulom <sboulom@gmail.com>
2020-09-27 19:55:02 -0700

    conditionals for dice game.Needs work

--- Dice Game/index.js
+++ Dice Game/index.js
@@ -16,16 +16,27 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
 
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!!!";
+} 
+else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!!!";
+}
+else {
+    document.querySelector("h1").innerHtml = "DRAW!!! TRY AGAIN";
+}
 
 
-
-
-
-
-
-
-
-
+// if (randomNumber1 > randomNumber2) {
+//     document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+//   }
+//   else if (randomNumber2 > randomNumber1) {
+//     document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+//   }
+//   else {
+//     document.querySelector("h1").innerHTML = "Draw!";
+//   }
+  
 
 
 
