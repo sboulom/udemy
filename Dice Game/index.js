99cf3eef1703d41e148b6e185aea04b6d6eeb2db
Steven Boulom <sboulom@gmail.com>
2020-09-27 18:54:26 -0700

    random number with img

--- Dice Game/index.js
+++ Dice Game/index.js
@@ -1,1 +1,12 @@
-var randomNumber1 = Math.floor(Math.random() * 6);
+var randomNumber1 = Math.floor(Math.random() * 6) +1;
+
+var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttributes("src", randomDiceImage);
+
+var randomNumber2 = Math.floor(Math.random() * 6) +1;
+
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
