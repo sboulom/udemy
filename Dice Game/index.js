c5ca301e1e7da6dc7792c4b3905ca661fa203b45
Steven Boulom <sboulom@gmail.com>
2020-09-27 19:21:58 -0700

    fixed both random image issues

--- Dice Game/index.js
+++ Dice Game/index.js
@@ -1,12 +1,15 @@
-var randomNumber1 = Math.floor(Math.random() * 6) +1;
+var randomNumber1 = Math.floor(Math.random() *6) +1; //+1 so you can round up to number 6
 
-var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+// var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/dice" + randomNumber1 + ".png"; // will become images/dice 1-6.png
 
 var image1 = document.querySelectorAll("img")[0];
-image1.setAttributes("src", randomDiceImage);
+
+image1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) +1;
 
-var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
-document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
