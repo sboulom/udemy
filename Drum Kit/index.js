7bfd95327541c9f433f0c09d59e848fe1da3171f
Steven Boulom <sboulom@gmail.com>
2020-09-28 20:14:45 -0700

    drum js practice

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,10 +1,37 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i < numberOfDrumButtons; i++) {
+for (var j = 0; j < numberOfDrumButtons; j++) {
     
-    document.querySelectorAll("button")[i].addEventListener("click", function (){
+    document.querySelectorAll("button")[j].addEventListener("click", function (){
 
-        alert("I got clicked");
-    
+        this.style.color = "white";
     });
 }
+
+
+
+
+
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
+
+//calculator
+// function add(num1, num2){
+//     return num1 + num2;
+// }
+ 
+// function multiply(num1, num2){
+//     return num1 * num2;
+// }
+
+// function divide(num1, num2){
+//     return num1 / num2;
+// }
+
+// function subtract(num1, num2){
+//     return num1 - num2;
+// }
+// function calculator(num1, num2, operator){
+//     return operator(num1, num2);
+// }
