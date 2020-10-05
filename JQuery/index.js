c5a95f188dfb0545b86d12844eda16bafbcbb9d9
Steven Boulom <sboulom@gmail.com>
2020-10-05 15:21:50 -0700

    jquery practice

--- JQuery/index.js
+++ JQuery/index.js
@@ -1,6 +1,9 @@
 
-$(document).ready(function(){
-    $("h1").css("color", "red");
-  
-    
-})
+
+//how to add settime out to JQuery
+$("h1").addClass("big-title margin-50")
+
+// setTimeout(function(){
+//     $("h1").removeClass("big-title")
+// }, 1500);
+
