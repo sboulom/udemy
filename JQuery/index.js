69d5a9aa1979914a08602668a72fc09666f4fbc3
Steven Boulom <sboulom@gmail.com>
2020-10-06 21:11:47 -0700

    jQuery

--- JQuery/index.js
+++ JQuery/index.js
@@ -3,7 +3,7 @@
 //how to add settime out to JQuery
 $("h1").addClass("big-title margin-50")
 
-// setTimeout(function(){
-//     $("h1").removeClass("big-title")
-// }, 1500);
+setTimeout(function(){
+    $("h1").removeClass("big-title")
+}, 1500);
 
