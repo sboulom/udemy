15044b9961c8e62e50b08be22bedbc3369a679a2
Steven Boulom <sboulom@gmail.com>
2020-10-09 23:37:52 -0700

    Jquery practice

--- JQuery/index.js
+++ JQuery/index.js
@@ -9,11 +9,11 @@ $("h1").addClass("big-title margin-50")
 
 
 
-// $("button").click(function(){
-//     $("h1").css("color", "purple");
-// });
+$("button").click(function(){
+    $("h1").css("color", "purple");
+});
 
-// $(document).keypress(function(event){
-//     $("h1").text(event.key);
-// });
+$(document).keypress(function(event){
+    $("h1").text(event.key);
+});
 
