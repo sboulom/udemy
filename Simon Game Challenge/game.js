527008735998a22de9ed9b83867137f4a23db382
Steven Boulom <sboulom@gmail.com>
2020-10-10 00:41:14 -0700

    simonChallenge step1/2

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -1,4 +1,4 @@
-
+//variables for colors
 var buttonColors = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
@@ -6,11 +6,11 @@ var gamePattern = [];
 
 function nextSequence(){
     console.log(randomNumber);
-    
+    //generate numbers 1-4
     var randomNumber = Math.floor(Math.random() * 4);
-    
+    //uses line 2 array and selects random number assinged in array
     var randomChosenColor = buttonColors[randomNumber];
-
+    //grabs game pattern array and pushes random chosen color---which is colors arrray + generated number
     gamePattern.push(randomChosenColor);
 }
 
