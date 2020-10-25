0fd4efd232fb11d69cf1608418408c0369bd1ccf
Steven Boulom <sboulom@gmail.com>
2020-10-25 00:26:41 -0700

    sequencing.

--- Simon Game Challenge/index.js
+++ Simon Game Challenge/index.js
@@ -7,6 +7,16 @@ function nextSequence(){
     var randomNumber = Math.floor(Math.random() *4);
     var randomChosenColor = buttonColor[randomNumber];
 
+
+    //adding random chosen color to end of array.
     gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+
+    //js way of playing sound. folder + variable to select file name + mp3 
+    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+
+    audio.play();
 
 }
