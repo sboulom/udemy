d9fe347a2d64ff8d869a23e3934ebd590810e7b7
Steven Boulom <sboulom@gmail.com>
2020-11-03 12:43:05 -0800

    added addition

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -14,10 +14,16 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
 
-    console.log(req.body);
+    // console.log(req.body.num1);
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    
+    var result = num1 + num2;
 
-    res.send("Thanks for posting");
+    res.send("The result of the calculator is " + result);
 });
+
+
 
 app.listen(3000, function(req, res){
     console.log("Server Started on PORT 3000!");
