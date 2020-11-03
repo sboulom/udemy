9d998673a767584f04ab81a7371976750937a54b
Steven Boulom <sboulom@gmail.com>
2020-11-03 13:09:52 -0800

    height and weight

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -24,6 +24,22 @@ app.post("/", function(req, res){
 });
 
 
+app.get("/bmiCalculator.html", function(req, res){
+
+    res.sendFile(__dirname + "/bmiCalculator.html");    
+});
+
+app.post("/", function(req, res){
+
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+
+    var bmiResult = 
+
+    res.send("Your BMI is " + bmiResult)
+})
+
+
 
 app.listen(3000, function(req, res){
     console.log("Server Started on PORT 3000!");
