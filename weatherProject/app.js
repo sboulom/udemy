a5222009ef68ced2573e4f59c3e3eb4038776bde
Steven Boulom <sboulom@gmail.com>
2020-11-07 11:32:03 -0800

    API weather

--- weatherProject/app.js
+++ weatherProject/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const https = require("https");
+
+
 
 const app = express();
 
 app.get("/", function(req, res){
-    res.send("HELLO server is up")
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=paris&units=imperial&appid=7980901759d3b1066013adeb63af74a7"
+
+    https.get(url, function(response){
+        console.log(response); 
+    })
+
+    res.send("HELLO server is up");
+
+
+
 })
 
 
