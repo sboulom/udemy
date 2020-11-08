cd319bf5ef11f16d7060f87eebeac2c823d241f3
Steven Boulom <sboulom@gmail.com>
2020-11-08 00:34:46 -0800

    added weather desciption.

--- weatherProject/app.js
+++ weatherProject/app.js
@@ -10,29 +10,22 @@ app.get("/", function(req, res){
     const url = "https://api.openweathermap.org/data/2.5/weather?q=paris&units=imperial&appid=7980901759d3b1066013adeb63af74a7"
 
     https.get(url, function(response){
-        console.log(response.statusCode); 
+        // console.log(response.statusCode); 
 
         response.on("data", function(data){
-        const weatherData = JSON.parse(data);
-        const object = {
-            name: "Steven",
-            favoriteFood: "Pizza",
-        }
-        console.log(JSON.stringify(object));
-        console.log(weatherData); 
 
+        const weatherData = JSON.parse(data);
+        const temp = weatherData.main.temp;
+
+        console.log(weatherData.weather[0].description); 
     });
     });
 
     res.send("HELLO server is up");
 });
 
-
-
-
-
-
-
 app.listen(3000, function(){
     console.log("Server is running on PORT 3000!")
-})
+});
+
+
