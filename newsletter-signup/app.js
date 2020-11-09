e4b9a4f1b29bc12ab00e3263c15924adbbe9e399
Steven Boulom <sboulom@gmail.com>
2020-11-08 23:05:22 -0800

    initial files

---
+++ newsletter-signup/app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const bodyParser = require("body-parser")
+const request = require("request")
+const app = express()
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server is running foo!")
+})
