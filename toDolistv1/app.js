acf2c80115b96e38f97af22d0478694c22222104
Steven Boulom <sboulom@gmail.com>
2020-11-19 12:07:24 -0800

    added ejs

--- toDolistv1/app.js
+++ toDolistv1/app.js
@@ -8,6 +8,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = [];
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -24,12 +26,15 @@ app.get("/", function(req, res){
     var day = today.toLocaleDateString("en-US", options);
 
 
-    res.render("lists", {kindOfDay: day})
+    res.render("lists", {kindOfDay: day, newListItems: items})
 })
 
 app.post("/", function(req, res){
-   var item = req.body.newItem
-   console.log(item);
+   var item = req.body.newItem;
+
+   items.push(item);
+
+   res.redirect("/");
 })
 
 app.listen(3000, function(){
