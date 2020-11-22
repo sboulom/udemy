e4404bcc8ac5a6dd3a8871dd7b34e7ca773463d6
Steven Boulom <sboulom@gmail.com>
2020-11-22 00:21:44 -0800

    more css styling

--- toDolistv1/app.js
+++ toDolistv1/app.js
@@ -12,7 +12,7 @@ var items = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
